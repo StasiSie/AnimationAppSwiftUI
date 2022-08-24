cb75ba676fe162bb133dd3ee232ca6dc1bf4613f
StasiSie <mail.koshaksa@gmail.com>
2022-08-24 19:46:32 +0300

    Initial Commit

---
+++ AnimationAppSwiftUI/AnimationAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationAppSwiftUIApp.swift
+//  AnimationAppSwiftUI
+//
+//  Created by Игорь Богданов on 24.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct AnimationAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimationAppSwiftUI
+//
+//  Created by Игорь Богданов on 24.08.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
